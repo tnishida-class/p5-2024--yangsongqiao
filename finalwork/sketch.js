d6892495ce79edb6b0900604e3082a4a8cf8c3b0
dewillis2 <146089461+dewillis2@users.noreply.github.com>
2024-11-21 13:09:24 +0900

    update

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,92 @@
 // 最終課題を制作しよう
+let fireWorks = [];
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  background(0, 0, 0, 25);
+  drawStars();
+  for (let i = fireWorks.length - 1; i >= 0; i--) {
+    fireWorks[i].update();
+    fireWorks[i].show();
+    if (fireWorks[i].isDone()) {
+      fireWorks.splice(i, 1);
+    }
+  }
 }
 
-function windowResized(){
+function mousePressed() {
+  let firework = new FireWork(mouseX, mouseY);
+  fireWorks.push(firework);
+}
+
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function drawStars() {
+  for (let i = 0; i < 20; i++) {
+    let x = random(0, windowWidth);
+    let y = random(0, windowHeight / 4);
+    fill(255, 255, 255);
+    ellipse(x, y, 5);
+  }
+}
+
+class FireWork {
+  constructor(x, y) {
+    this.particles = [];
+    for (let i = 0; i < 100; i++) {
+      this.particles.push(new Particle(x, y));
+    }
+  }
+
+  update() {
+    for (let particle of this.particles) {
+      particle.update();
+    }
+  }
+
+  show() {
+    for (let particle of this.particles) {
+      particle.show();
+    }
+  }
+
+  isDone() {
+    for (let particle of this.particles) {
+      if (!particle.isDone()) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-10, 10);
+    this.vy = random(-10, 10);
+    this.lifespan = 255;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.lifespan -= 4;
+  }
+
+  show() {
+    noStroke();
+    fill(random(0, 255), random(0, 255), random(0, 255), this.lifespan);
+    ellipse(this.x, this.y, 5);
+  }
+
+  isDone() {
+    return this.lifespan <= 0;
+  }
 }
--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(200, 400); // 60以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -18,7 +18,15 @@ function setup(){
   fill(0);
   const dx = width / scores.length;
   let px, py; // 線を引くために一つ前の点を覚えておく変数
-  for(let i = 0; i < scores.length; i++){
+  for(let i = 0; i < scores.length; i++)
+  {
     // BLANK[1]
+    ellipse(dx*i, height-scores[i],10,10);
+    if(i != 0)
+    {
+      line(px, py, dx*i, height - scores[i]);
+    }
+    px = dx*i;
+    py = height - scores[i];
   }
 }
